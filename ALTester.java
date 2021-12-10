9e77a9268b23a619f00cf76e7a32c5d4fd3c1c23
Ruiwen-T <you@example.com>
2021-12-10 13:19:00 -0500

    dec 10 cw

---
+++ ALTester.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class ALTester{
+	public static boolean isSorted(ArrayList<Integer> al){
+		return false;
+	}
+	public static void main(String[] args){
+		ArrayList<Integer> oal = new ArrayList<Integer>();
+		
+		
+	}
+	
+}
